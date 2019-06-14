9000c87aafab65cff741bd8f735ee96660ca6107
Aaron Scott <aa5278sc-s@student.lu.se>
2019-06-15 01:16:31 +0200

    update/nightly dev save

--- setup.py
+++ setup.py
@@ -2,6 +2,20 @@ from setuptools import setup
 
 version = '0.0.01'
 
-namespace_packages = ['grollup']
+namespace_packages = []
 
-scripts = ['grollup/bin/ghostrollup']
+packages = ['grollup']
+
+scripts = ['bin/ghostrollup']
+
+install_requires = []
+
+setup(name='ghostrollup',
+      version=version,
+      namespace_packages=namespace_packages,
+      packages=packages,
+      scripts=scripts,
+      license='open',
+      author='Aaron Scott',
+      author_email='aa5278sc-s@student.lu.se',
+      install_requires=install_requires)
